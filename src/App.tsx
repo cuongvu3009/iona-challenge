c326d9a99cdef632b900508de63966113dcbdfa9
cuongvu3009 <cuongvu3009@gmail.com>
2022-11-09 09:37:03 +0200

    feat: routing implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Details from './pages/Details';
+import Favorites from './pages/Favorites';
+import About from './pages/About';
+
 function App() {
-  return <div className='App'>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/favorites' element={<Favorites />} />
+        <Route path='/:id' element={<Details />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
